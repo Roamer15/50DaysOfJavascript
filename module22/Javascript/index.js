8ca1b756ac827ca7432fdcd4d082192bd0e40c08
Roamer15 <ianseeksbeta@gmail.com>
2025-01-07 00:02:03 +0100

    Module 22 complete

--- module22/Javascript/index.js
+++ module22/Javascript/index.js
@@ -1,83 +1,23 @@
-const APIURL = 'https://api.github.com/users/'
+const buttons = document.querySelectorAll('.btn')
 
-const mainContent = document.getElementById('main-content')
-const form = document.getElementById('form')
-const search = document.getElementById('search')
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        const x = e.clientX
+        const y = e.clientY
 
-async function getUser(username) {
-    try {
-        const { data } = await axios(APIURL + username)
-        createUserCard(data)
-        getRepos(username)
-    } catch (err) {
-        if (err.response.status === 404) {
-            createErrorCard('No profile with this username')
-        }
-    }
-}
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
 
-async function getRepos(username) {
-    try {
-        const { data } = await axios(APIURL + username + '/repos?sort=created')
-        addReposToCard(data)
-    } catch (error) {
-        createErrorCard('Problem fetching repos')
-    }
-}
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
 
-function createUserCard(user) {
-    const userID = user.name || user.login
-    const userBio = user.bio ? `<p>${user.bio}</p>` : ''
-    const cardHTML = `
-        <div class="card">
-            <div>
-                <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
-            </div>
-            <div class="user-info">
-                <h2>${userID}</h2>
-                <p>${userBio}</p>
-                <ul>
-                    <li>${user.followers} <strong>Followers</strong></li>
-                    <li>${user.following} <strong>Following</strong></li>
-                    <li>${user.public_repos} <strong>Repos</strong></li>
-                </ul>
-                <div id="repos"></div>
-            </div>
-        </div>
-    `
-    mainContent.innerHTML = cardHTML
-}
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
 
-function createErrorCard(message) {
-    const cardHTML = `
-        <div class="card">
-            <h1>${message}</h1>
-        </div>
-    `
-    mainContent.innerHTML = cardHTML
-}
+        this.appendChild(circle)
 
-function addReposToCard(repos) {
-    const reposElement = document.getElementById('repos')
-    repos.slice(0, 10).forEach(repo => {
-        const repoElement = document.createElement('a')
-        repoElement.classList.add('repo')
-        repoElement.href = repo.html_url
-        repoElement.target = '_blank'
-        repoElement.innerText = repo.name
-        reposElement.appendChild(repoElement)
+        setTimeout(() => circle.remove(), 500)
     })
-}
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault()
-
-    const username = search.value
-
-    if (username) {
-        getUser(username)
-
-        search.value = ''
-    }
 })
-
